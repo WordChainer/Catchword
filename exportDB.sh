120f3283bdb528676cfb54062dce070cf396bd90
xCrypt0r <legacy5877@gmail.com>
2020-12-27 05:04:04 +0000

    Add script to export db

---
+++ exportDB.sh
@@ -0,0 +1,2 @@
+mongoexport -d admin -c words -f value -q='{ "length": 3 }' --sort='{ value: 1 }' --noHeaderLine --type=csv -o words.txt
+mongoexport -d admin -c words -f value -q='{ "length": 2 }' --sort='{ value: 1 }' --noHeaderLine --type=csv -o words2.txt
