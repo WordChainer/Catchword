322bd94c3e286a6877a4466fcc8a5e0530990f45
xCrypt0r <legacy5877@gmail.com>
2021-01-10 11:16:12 +0000

    Update routes/manage.js

--- routes/manage.js
+++ routes/manage.js
@@ -4,14 +4,19 @@ const UserModel = require('../models/User.js');
 const searchLogModel = require('../models/SearchLog.js');
 
 router.get('/', async (req, res) => {
+    if (!res.locals.isAdmin) {
+        return res.send('접근 권한이 없습니다!');
+    }
+
+    let users = await UserModel.find().sort({ date: 1 }),
+        searchCounts = await searchLogModel.aggregate([
+            { $group: { _id: '$user',  count: { $sum: 1 } } }
+        ]);
+
     res.render('manage', {
         moment: require('moment'),
-        users: await UserModel
-            .find()
-            .sort({ date: 1 }),
-        searchCounts: await searchLogModel.aggregate([
-            { $group: { _id: "$user",  count: { $sum: 1 } } }
-        ])
+        users,
+        searchCounts
     });
 });
 
