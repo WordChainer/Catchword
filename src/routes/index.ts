b9951fb8e62b211f4a1ce26b852ab8c2dc36d357
xCrypt0r <legacy5877@gmail.com>
2021-05-21 14:48:11 +0000

    Update main page design

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,25 @@
 import { Router, Request, Response } from 'express';
 import glob from 'fast-glob';
+import User from '../models/User.model';
 import Word from '../models/Word.model';
+import SearchLog from '../models/SearchLog.model';
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
     let slides = await glob('images/slide*.jpg', { cwd: 'public' });
-    let counts = await Promise.all([
-        Word.countDocuments({}),
-        Word.countDocuments({ isHidden: true }),
-        Word.countDocuments({ length: 3 }),
-        Word.countDocuments({ length: 2 })
+    let sample = await Word.aggregate([
+        { $match: { isHidden: false } },
+        { $sample: { size: 1 } }
+    ]);
+    let contents = await Promise.all([
+        Word.countDocuments(),
+        User.countDocuments(),
+        SearchLog.countDocuments(),
+        sample[0].value
     ]);
 
-    res.render('index', { slides, counts });
+    res.render('index', { slides, contents });
 });
 
 export default router;
