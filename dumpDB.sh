1f8de499768fcd7d2989b891fb5d24f9b9be13fd
xCrypt0r <legacy5877@gmail.com>
2021-07-22 17:51:52 +0000

    Add feature to delete files older than 7 days

--- dumpDB.sh
+++ dumpDB.sh
@@ -1,1 +1,4 @@
-mongodump -d admin --gzip --archive > /home/ubuntu/Catchword/backup/`date -d "9 hours" +"%Y%m%d_%H%M%S".gz`
+FOLDER_PATH='/home/ubuntu/Catchword/backup/'
+
+mongodump -d admin --gzip --archive > ${FOLDER_PATH}`date -d "9 hours" +"%Y%m%d_%H%M%S".gz`
+find ${FOLDER_PATH} -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
