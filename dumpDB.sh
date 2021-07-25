760e1d94545e73ddd391a32c11d10c118127e34c
xCrypt0r <legacy5877@gmail.com>
2021-07-25 18:35:13 +0000

    Add shebang

--- dumpDB.sh
+++ dumpDB.sh
@@ -1,4 +1,6 @@
-FOLDER_PATH='/home/ubuntu/Catchword/backup/'
+#!/bin/bash
 
-mongodump -d admin --gzip --archive > ${FOLDER_PATH}`date -d "9 hours" +"%Y%m%d_%H%M%S".gz`
+FOLDER_PATH="$(dirname $(realpath "$0"))/backup/"
+
+mongodump -d admin --gzip --archive > ${FOLDER_PATH}$(date -d "9 hours" +"%Y%m%d_%H%M%S".gz)
 find ${FOLDER_PATH} -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
