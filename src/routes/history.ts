d18e16fb761e6d9c2ab07621c3786b9cf6e25e04
xCrypt0r <legacy5877@gmail.com>
2021-08-12 04:57:08 +0000

    Add login restriction to history page

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -5,6 +5,10 @@ import EditLogController from '../controllers/EditLog.controller';
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
+    if (!req.user) {
+        return res.status(400).send('로그인 후 이용가능합니다!');
+    }
+
     res.render('history', {
         dayjs,
         editLogs: await EditLogController.GetAllEditLogs()
