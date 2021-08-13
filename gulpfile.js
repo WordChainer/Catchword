b20d1e02fa9ececd6d5f88ad71cf5735d8e90183
xCrypt0r <legacy5877@gmail.com>
2021-08-13 16:28:44 +0000

    Split manual.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp  = require('gulp');
+const minifyHTML = require('gulp-htmlmin');
 const minifyJS = require('gulp-uglify-es').default;
 const minifyCSS = require('gulp-clean-css');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-image');
 const rename = require('gulp-rename');
+
+gulp.task('minify-html', () => {
+    return gulp.src('lib/html/*.html')
+        .pipe(minifyHTML({ collapseWhitespace: true }))
+        .pipe(gulp.dest('public/html'));
+});
 
 gulp.task('minify-js', () => {
     return gulp.src('lib/js/*.js')
@@ -27,8 +34,9 @@ gulp.task('minify-image', () => {
 });
 
 gulp.task('watch', () => {
+    gulp.watch('lib/html/*.html', gulp.series('minify-html'));
     gulp.watch('lib/js/*.js', gulp.series('minify-js'));
     gulp.watch('lib/scss/**/*.scss', gulp.series('minify-css'));
 });
 
-gulp.task('default', gulp.series('minify-js', 'minify-css'));
+gulp.task('default', gulp.series('minify-html', 'minify-js', 'minify-css'));
