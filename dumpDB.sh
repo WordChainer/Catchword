ca20d1d8ac7ca95c34b5e96c7a0a4eee20e3c66b
xCrypt0r <legacy5877@gmail.com>
2021-10-12 17:29:19 +0000

    Add double quotes

--- dumpDB.sh
+++ dumpDB.sh
@@ -2,5 +2,5 @@
 
 FOLDER_PATH="$(dirname $(realpath "$0"))/backup/"
 
-mongodump -d admin --gzip --archive > ${FOLDER_PATH}$(date -d "9 hours" +"%Y%m%d_%H%M%S".gz)
-find ${FOLDER_PATH} -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
+mongodump -d admin --gzip --archive > "$FOLDER_PATH$(date -d "9 hours" +"%Y%m%d_%H%M%S".gz)"
+find "$FOLDER_PATH" -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
