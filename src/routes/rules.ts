747cc2052903fac4aa09539d72e510d7eee44de4
xCrypt0r <legacy5877@gmail.com>
2021-10-13 11:39:55 +0000

    Add rules page

---
+++ src/routes/rules.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    res.render('rules');
+});
+
+export default router;
